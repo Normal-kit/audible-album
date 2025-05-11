12619c25c9f18d5ea6d5dc3115898c917e289a8f
none123 <76775605+TIM-ai-coder@users.noreply.github.com>
2025-05-11 18:07:54 +0900

    녹음기능, 로컬저장기능 구현

--- front/myapp/lib/CameraPage.dart
+++ front/myapp/lib/CameraPage.dart
@@ -1,10 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart'; // for basename()
-import 'dart:io';
+import 'package:path/path.dart';
 
 class Camerapage extends StatefulWidget {
   const Camerapage({super.key});
@@ -25,7 +25,7 @@ class _CamerapageState extends State<Camerapage> {
 
   Future<void> _pickImageFromCamera() async {
     setState(() {
-      _gptResult = '이미지를 분석 중입니다...'; // 분석 시작 전 메시지 초기화
+      _gptResult = '이미지를 분석 중입니다...';
     });
 
     final picker = ImagePicker();
@@ -33,28 +33,20 @@ class _CamerapageState extends State<Camerapage> {
 
     if (pickedFile != null) {
       final tempImage = File(pickedFile.path);
-
-      //앱 전용 디렉토리에 저장
       final appDir = await getApplicationDocumentsDirectory();
       final fileName = basename(pickedFile.path);
       final savedImage = await tempImage.copy('${appDir.path}/$fileName');
-      print('이미지 저장 경로: ${savedImage.path}');
+
       setState(() {
         _imageFile = savedImage;
       });
 
       await _analyzeImageWithGPT(savedImage);
-    } else {}
+    }
   }
 
   Future<void> _analyzeImageWithGPT(File image) async {
-    setState(() {
-      _gptResult = '이미지를 분석 중입니다...';
-    });
-
-    final uri = Uri.parse(
-      'http://223.130.160.126:8000/upload/photo',
-    ); // FastAPI 서버 주소
+    final uri = Uri.parse('http://223.130.160.126:8000/upload/photo');
 
     final request = http.MultipartRequest('POST', uri)
       ..files.add(await http.MultipartFile.fromPath('photo', image.path));
@@ -85,118 +77,114 @@ class _CamerapageState extends State<Camerapage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          _imageFile != null
-              ? Column(
-                children: [
-                  Expanded(
-                    flex: 5,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
+      body: _imageFile != null
+          ? Column(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Container(
+                      margin: const EdgeInsets.only(top: 100),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.file(
+                          _imageFile!,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 100.0),
+                Expanded(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Align(
+                      alignment: Alignment.topCenter,
                       child: Container(
-                        margin: const EdgeInsets.only(top: 100),
-                        child: ClipRRect(
+                        height: 150,
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
                           borderRadius: BorderRadius.circular(8),
-                          child: Image.file(
-                            _imageFile!,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
+                        ),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          _gptResult,
+                          style: const TextStyle(fontSize: 16),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 4,
                         ),
                       ),
                     ),
                   ),
-
-                  SizedBox(height: 100.0),
-
-                  Expanded(
-                    flex: 3,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Align(
-                        alignment: Alignment.topCenter,
-                        child: Container(
-                          height: 150,
-                          padding: const EdgeInsets.all(12),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.grey),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          alignment: Alignment.topLeft,
-                          child: Text(
-                            _gptResult,
-                            style: const TextStyle(fontSize: 16),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 4,
-                          ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 16, right: 16, bottom: 24, top: 12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  '/record',
+                                  arguments: {
+                                    'imagePath': _imageFile!.path,
+                                    'gptResult': _gptResult,
+                                  },
+                                );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: const Size(100, 100),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                backgroundColor: Colors.red,
+                              ),
+                              child: const Text(
+                                '녹음',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: _pickImageFromCamera,
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: const Size(100, 100),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                backgroundColor: Colors.blue,
+                              ),
+                              child: const Text(
+                                '재촬영',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
+                      ],
                     ),
                   ),
-
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 16,
-                        right: 16,
-                        bottom: 24,
-                        top: 12,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  Navigator.pushNamed(context, '/record');
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  fixedSize: Size(100, 100),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ),
-                                  backgroundColor: Colors.red,
-                                ),
-                                child: const Text(
-                                  '녹음',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-
-                              ElevatedButton(
-                                onPressed: () {
-                                  _pickImageFromCamera();
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  fixedSize: Size(100, 100),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ),
-                                  backgroundColor: Colors.blue,
-                                ),
-                                child: const Text(
-                                  '재촬영',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              )
-              : const Center(child: Text('이미지를 불러오는 중...')),
+                ),
+              ],
+            )
+          : const Center(child: Text('이미지를 불러오는 중...')),
     );
   }
 }
